6b4cb0cda150d38c3eeb4af8ab0e6f5b2d424f16
KALYANISHARNAGATCS <sharnagatkalyani0812@gmail.com>
2025-05-12 14:40:58 +0530

    h2  style added

--- JS/script.js
+++ JS/script.js
@@ -38,23 +38,7 @@ window.onload = () => {
 };
 
 
-function data() {
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-    var phone = document.getElementById("phone").value;
-    var address = document.getElementById("address").value;
 
-    if (name === "" || email === "" || phone === "" || address === "") {
-        alert("All fields are mandatory!");
-        return false;
-    } else if (phone.length !== 10 || isNaN(phone)) {
-        alert("Phone number should be exactly 10 digits and numeric!");
-        return false;
-    } else {
-        alert("Form submitted successfully!");
-        return true;
-    }
-}
 AOS.init();
 
 document.addEventListener("DOMContentLoaded", function () {
